3f5400037e34353f5b902df885405631aefebd04
Camilo H <chernandez@unimayor.edu.co>
2022-04-23 22:14:09 -0500

    Commit excercise JB Season 1

---
+++ Java Basico/Sesion 1/src/package_main/Main.java
@@ -0,0 +1,16 @@
+package package_main;
+
+public class Main {
+    public static void main(String[] args) {
+        int numero = 12;
+        long numero1 = 127998;
+        double numero2 = 12.3;
+        boolean decision = true;
+        String cadena = "Hola mundo!";
+        System.out.println("Tipo int "+numero);
+        System.out.println("Tipo long "+numero1);
+        System.out.println("Tipo double "+numero2);
+        System.out.println("Tipo boolean "+decision);
+        System.out.println("Tipo cadena "+cadena);
+    }
+}
